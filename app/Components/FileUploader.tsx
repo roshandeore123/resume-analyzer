edbc175f75832d8af6b597a65aebce75e3347205
roshan <roshandeore702@gmail.com>
2025-09-17 23:03:39 +0530

    first commit

---
+++ app/Components/FileUploader.tsx
@@ -0,0 +1,74 @@
+import {  useState,useCallback } from "react"
+import {useDropzone} from 'react-dropzone'
+import { formatSize } from "~/lib/utils";
+
+interface FileUploaderProps{
+    onFileSelect?:(file:File|null)=>void;
+}
+const FileUploader = ({onFileSelect}:FileUploaderProps) => {
+    //const [file, setFile]=useState();
+
+    const onDrop = useCallback((acceptedFiles :File[])=> {
+    const File=acceptedFiles[0] || null;
+
+    onFileSelect ?.(File);
+  }, [onFileSelect]);
+
+  const maxFileSize=20*1024*1024;
+
+  const {getRootProps, getInputProps, isDragActive,acceptedFiles} = useDropzone({onDrop,
+    multiple:false,
+    accept:{'application/pdf':['.pdf']},
+    maxSize:maxFileSize,
+})
+const File=acceptedFiles[0] || null;
+  return (
+    <div className='w-full gradient-border'> 
+     <div {...getRootProps()}>
+      <input {...getInputProps()} />
+
+      <div className="space-y-4 cursor-pointer">
+       
+        {File ?(
+          <div className="uploader-selected-file" onClick={(e)=>e.stopPropagation()}>
+             <img src="/images/pdf.png" alt="pdf" className="size-10 " />
+             <div className="flex item-center space-x-3">
+             
+             <div>
+                <p className="text-sm font-medium  text-gray-700 truncate max-w-xs">
+                    {File.name}
+                </p>
+                 <p className="text-sm text-gray-500">
+                  {formatSize(File.size)}
+                  </p>
+             </div>
+           </div>
+            <button className="p-2 cursor-pointer" onClick={(e)=>{
+              onFileSelect?.(null)
+            }}>
+              <img src="/icons/cross.svg " alt="remove" className="w-4 h-4"/>
+            </button>
+          </div>
+          
+        ):(
+            <div>
+               <div className="mx-auto w-16 h-16 flex itema-center justify-center mb-2">
+                 <img src="/icons/info.svg" alt="upload" className="size-20"/>
+
+               </div>
+                <p className="text-lg text-gray-500">
+                    <span className="font-semibold">
+                        Click to upload
+                    </span>or drag amd drop
+
+                </p>
+                <p className="text-lg text-gray-500"> PDF (max {formatSize(maxFileSize)})</p>
+            </div> 
+        )}
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default FileUploader
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,47 @@
+import { resumes } from "~/Constants";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/Components/Navbar";
+import ResumeCard from "~/Components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "resumind" },
+    { name: "description", content: "smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  const { auth } =usePuterStore();
+  const navigate=useNavigate();
+  
+  useEffect(()=>{
+    if (!auth.isAuthenticated) navigate('/auth?next=/');
+
+  },[auth.isAuthenticated])
+
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+        <Navbar/>
+          <section className="main-section ">
+        <div className="page-heading py-16">
+          <h1>Track your applications & resume rating </h1>
+          <h2>Review your submission and check AI-powered feedback.</h2>
+        </div>
+      
+       
+       {resumes.length > 0  &&(
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume}/>
+      ))}
+        </div>
+
+
+       )}
+       </section>
+     
+  </main>;
 }
